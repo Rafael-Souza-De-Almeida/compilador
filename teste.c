46df94d2d3403a779aa14bcd1e6c7fef5a9a7374
Rafael-Souza-De-Almeida <rafaelalmeida37@ufrrj.br>
2025-04-06 23:34:49 -0300

    Initial commit

---
+++ teste.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include <stdio.h>
+int main(void) {
+
+    int t1,t2,t3,t4,a,valor;
+
+        t1 = 5;
+        a = t1;
+        t2 = a;
+        t3 = 2;
+        t4 = t2 * t3;
+        valor = t4;
+        printf("%d\n\t", valor);
+        return 0;
+}
