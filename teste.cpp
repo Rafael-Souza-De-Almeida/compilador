f061d1809928264b518ebe2db3b0941c44f9d791
leoaizhu <jinglileo00@gmail.com>
2025-06-08 10:42:56 -0300

    Adicionando cpp

---
+++ teste.cpp
@@ -0,0 +1,23 @@
+/* Compilador hahaha */
+#include <iostream>
+#include <string.h>
+#include <stdio.h>
+using namespace std; 
+int main(void) {
+	int t1;
+	int t2;
+	int t3;
+	int t4;
+	int __v2;
+	int __v1;
+	int __v0;
+	t1 = 5;
+	__v0 = t1;
+	t2 = __v0;
+	 cout << t2 << endl;
+	t3 = 3;
+	__v1 = t3;
+	t4 = 2;
+	__v2 = t4;
+	return 0;
+}
